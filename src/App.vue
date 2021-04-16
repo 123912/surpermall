51622117461c9689b872a31475f4be3ef30ce4a4
用123912 <1907231867@qq.com>
2021-04-16 15:37:18 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Description: 
+ * @Author: Zengxs
+ * @Date: 2021-04-16 15:08:49
+ * @LastEditors: Zengxs
+ * @LastEditTime: 2021-04-16 15:35:50
+-->
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
